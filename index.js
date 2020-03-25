d869bb6ade83c829b2f89b2b83ab9277d6427c96
Brian Nicholls <nichol88@gmail.com>
2020-03-25 20:38:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ document.addEventListener('keydown', function(e) {
 });
 
 function moveDodgerRight() {
-  let rightNumbers = dodger.style.left.replace('px','');
+  let leftNumbers = dodger.style.left.replace('px','');
   let left = parseInt(leftNumbers, 10);
 
   if (left < 400) {
